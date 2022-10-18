ed3e658031ed947659291000f60f0a43ef3980df
buzzfi5 <116114150+buzzfi5@users.noreply.github.com>
2022-10-18 15:42:38 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "19233893"; // your league ID
-export const leagueName = "Bush League ©"; // your league name
+export const leagueName = "Bush League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
